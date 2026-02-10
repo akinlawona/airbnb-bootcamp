a4b5574a7815c4bbe2aeb67d34952f34776839d3
Akin Adejuwon <adejuwonakinlawon@yahoo.com>
2026-02-10 05:49:20 +0100

    fix use search params suspense

--- src/app/(protected)/hosting/page.tsx
+++ src/app/(protected)/hosting/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { Suspense } from "react";
 import HostingClient from "./HostingClient";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {};
 
@@ -9,10 +10,23 @@ const page = (props: Props) => {
   const upcomingReservations = [{}];
 
   return (
-    <HostingClient
-      todaysReservations={todaysReservations}
-      upcomingReservations={upcomingReservations}
-    />
+    <Suspense
+      fallback={
+        <div className="space-y-4 p-6">
+          <Skeleton className="h-12 w-64" />
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <Skeleton className="h-48 w-full" />
+            <Skeleton className="h-48 w-full" />
+            <Skeleton className="h-48 w-full" />
+          </div>
+        </div>
+      }
+    >
+      <HostingClient
+        todaysReservations={todaysReservations}
+        upcomingReservations={upcomingReservations}
+      />
+    </Suspense>
   );
 };
 
